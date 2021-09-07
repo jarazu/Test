fc7eff52bb4d98834730082cc5fa9988dbf04aec
Razu <jahangir8988@gmail.com>
2021-09-08 01:37:37 +0600

    okay

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,10 +2,18 @@ using DLL;
 using DLL.DBContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Test
 {
